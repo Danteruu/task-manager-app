52b3c925c28d86f2bac02029c9d6cf525e54ef5f
Danteruu <filip.chojna@nexteer.com>
2021-05-16 19:18:54 +0200

    Add .gitignore

--- src/app/models/task-list.model.ts
+++
@@ -1,6 +0,0 @@
-export class TaskList {
-    id!: number;
-    name!: string;
-    createDate!: Date;
-    deleted!: boolean;
-  }
--- src/app/models/task.model.ts
+++
@@ -1,8 +0,0 @@
-export class Task {
-    id!: number;
-    taskListId!: number;
-    text!: string;
-    createDate = new Date(2021, 5, 15);
-    completed = false;
-    deleted = false;
-  }
